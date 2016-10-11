f138655d64ec488d524b85cc078bc151c5a17759
Baron Alloway <baron@alloway.net>
2016-10-11 14:03:47 -0400

    got sass working. added resources folder. general customization of header block.

--- index.php
+++ index.php
@@ -4,12 +4,29 @@
 <html>
 <head>
 <title>Baron Alloway | baronalloway.com</title>
+<!-- metas -->
+<meta name="author" content="Baron Alloway" />
+<meta name="description" content="The personal Website of Baron Alloway." />
+<meta name="keywords"  content="baron,alloway,baronalloway,baron alloway" />
+<!--Bootstrap -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="stylesheets/style.css">
 </head>
 <body>
+    <!-- Cover -->
+    <div class="intro">
+        <div class="scrollable">
+            <center><img class="logo" src="resources/logo-1.png"></center>
+        </div>
+    </div>
+    <div class="infotext">
+        Baron Alloway. A Tinkerer and Programmer from Toronto, Canada.<br>
+        Currently Studying Biomedical Engineering in Miami, Florida.
 
-
-
-
+    </div>
 </body>
 <footer></footer>
 </html>
