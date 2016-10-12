0a8ca88c639c40305e0fee700dd8cbdc4b7dd69e
Baron Alloway <baron@alloway.net>
2016-10-12 12:10:02 -0400

    More Style. Typekit. Lots of columns. Front page is done rough draft...moving onto other pages now.

--- index.php
+++ index.php
@@ -14,6 +14,11 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" href="stylesheets/style.css">
+<!-- adobe typekit -->
+<script src="https://use.typekit.net/zri6lry.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<!-- font awesome -->
+<script src="https://use.fontawesome.com/9c18fa612c.js"></script>
 </head>
 <body>
     <!-- Cover -->
@@ -22,23 +27,73 @@
             <center><img class="logo" src="resources/logo-1.png"></center>
         </div>
     </div>
-
-    <div class="infotext">
+<div class="breaker"></div>
+    <div class="infotext text">
         <div class="container">
         <div class="col-md-6"><center><img class="me" src="resources/avatar.jpg"></center>
         </div>
-        <div class="col-md-6">
-        Baron Alloway. A Tinkerer and Programmer from Toronto, Canada.<br>
-        Currently Studying Biomedical Engineering in Miami, Florida.
+        <div class="col-md-6 center">
+        <h1>Baron Alloway.</h1><br>A Tinkerer and Programmer from Toronto, Canada.&#x1f1e8;&#x1f1e6;<br>
+        Currently Studying Biomedical Engineering in Miami, Florida.&#x1f1fa;&#x1f1f8;
         </div>
     </div>
     </div>
     <div class="breaker"></div>
     <div class="hotlinks">
-        <div class="col-md-4">Photography</div>
-        <div class="col-md-4">Recent Projects</div>
-        <div class="col-md-4">Read my Blog</div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="bordered">
+                        <h2>Photography</h2>
+                        <p>I take photos sometimes. They turn out okay.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="bordered">
+                        <h2>Recent Projects</h2>
+                        <p>A half hour of code a day is way more therapeutic than a day at the spa for me. </p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="bordered right">
+                        <h2>Read my Blog</h2>
+                        <p>Rants, Raves, Praises, and Public Shamings happen here.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
-<footer></footer>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 social-links">
+            <h2>Baron Alloway</h2>
+            <i class="fa fa-facebook fa-2x"></i>
+            <i class="fa fa-twitter fa-2x"></i>
+            <i class="fa fa-instagram fa-2x"></i>
+            <i class="fa fa-github fa-2x"></i>
+
+            </div>
+            <div class="col-md-4">
+            </div>
+            <div class="col-md-4 right">
+                <!-- Begin MailChimp Signup Form -->
+<div id="mc_embed_signup">
+<form action="//baronalloway.us8.list-manage.com/subscribe/post?u=9484c171227fad29bf7e11248&amp;id=dc2e95ee58" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<label for="mce-EMAIL" class="social-links">Subscribe to my private mailing list.</label>
+	<input type="email" value="" name="EMAIL" class="email-input" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_9484c171227fad29bf7e11248_dc2e95ee58" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+            </div>
+        </div>
+    </div>
+</footer>
 </html>
