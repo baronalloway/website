9ccaf8b3fa7b4c4a0f7a3885955b9305ecab2972
Baron Alloway <baron@alloway.net>
2016-10-12 15:08:35 -0400

    split footer into its own php file. Added a second parallax section and began to style the homepage.

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" href="stylesheets/style.css">
+<link rel="stylesheet" href="stylesheets/media-listeners.css">
 <!-- adobe typekit -->
 <script src="https://use.typekit.net/zri6lry.js"></script>
 <script>try{Typekit.load({ async: true });}catch(e){}</script>
@@ -63,37 +64,9 @@
             </div>
         </div>
     </div>
+    <div class="parallax">
+    </div>
 </body>
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 social-links">
-            <h2>Baron Alloway</h2>
-            <i class="fa fa-facebook fa-2x"></i>
-            <i class="fa fa-twitter fa-2x"></i>
-            <i class="fa fa-instagram fa-2x"></i>
-            <i class="fa fa-github fa-2x"></i>
-
-            </div>
-            <div class="col-md-4">
-            </div>
-            <div class="col-md-4 right">
-                <!-- Begin MailChimp Signup Form -->
-<div id="mc_embed_signup">
-<form action="//baronalloway.us8.list-manage.com/subscribe/post?u=9484c171227fad29bf7e11248&amp;id=dc2e95ee58" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<label for="mce-EMAIL" class="social-links">Subscribe to my private mailing list.</label>
-	<input type="email" value="" name="EMAIL" class="email-input" id="mce-EMAIL" placeholder="email address" required>
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_9484c171227fad29bf7e11248_dc2e95ee58" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    </div>
-</form>
-</div>
-
-<!--End mc_embed_signup-->
-            </div>
-        </div>
-    </div>
+    <?php include 'page-components/footer.php';?>
 </footer>
 </html>
