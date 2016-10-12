fef20c62a946bac67080716a447906f0506022d8
Baron Alloway <baron@alloway.net>
2016-10-12 17:56:49 -0400

    broke scripts, head, footer, css links into separate php files and then we include them in the pages. Began work on blog and projects pages.

---
+++ blog/index.php
@@ -0,0 +1,15 @@
+<HTML>
+<head>
+    <title>Blog | baronalloway.com</title>
+    <?php include '../page-components/scriptsnthings.php'; ?>
+</head>
+<body>
+    <?php include '../page-components/header.php';?>
+
+
+
+
+
+
+    <?php include '../page-components/footer.php';?>
+</body>
--- index.php
+++ index.php
@@ -8,18 +8,7 @@
 <meta name="author" content="Baron Alloway" />
 <meta name="description" content="The personal Website of Baron Alloway." />
 <meta name="keywords"  content="baron,alloway,baronalloway,baron alloway" />
-<!--Bootstrap -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="stylesheets/style.css">
-<link rel="stylesheet" href="stylesheets/media-listeners.css">
-<!-- adobe typekit -->
-<script src="https://use.typekit.net/zri6lry.js"></script>
-<script>try{Typekit.load({ async: true });}catch(e){}</script>
-<!-- font awesome -->
-<script src="https://use.fontawesome.com/9c18fa612c.js"></script>
+<?php include 'page-components/scriptsnthings.php'; ?>
 </head>
 <body>
     <!-- Cover -->
@@ -44,19 +33,19 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-4">
-                    <div class="bordered">
+                    <div class="bordered" onclick="location.href='http://one6.one'">
                         <h2>Photography</h2>
                         <p>I take photos sometimes. They turn out okay.</p>
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="bordered">
+                    <div class="bordered" onclick="location.href='/projects'">
                         <h2>Recent Projects</h2>
-                        <p>A half hour of code a day is way more therapeutic than a day at the spa for me. </p>
+                        <p>A half hour of code is way more therapeutic than a day at the spa for me. </p>
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="bordered right">
+                    <div class="bordered right" onclick="location.href='/blog'">
                         <h2>Read my Blog</h2>
                         <p>Rants, Raves, Praises, and Public Shamings happen here.</p>
                     </div>
@@ -65,6 +54,11 @@
         </div>
     </div>
     <div class="parallax">
+        <h3>Follow me on the web.<br><br>
+            <a href="https://facebook.com/baronallowayofficial"><i class="fa fa-facebook fa-2x uniformpadding"></i></a>
+            <a href="https://twitter.com/baronalloway"><i class="fa fa-twitter fa-2x uniformpadding"></i></a>
+            <a href="https://instagram.com/baronalloway"><i class="fa fa-instagram fa-2x uniformpadding"></i></a>
+            <a href="https://github.com/baronalloway"><i class="fa fa-github fa-2x uniformpadding"></i></a>
     </div>
 </body>
     <?php include 'page-components/footer.php';?>
